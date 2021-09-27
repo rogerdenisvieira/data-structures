48b64d7f5d0602ae666ceb53d6e341ebbc2abbdf
Roger Vieira <roger_v@dell.com>
2021-09-27 15:23:04 -0300

    Added Quicksort

---
+++ Quicksort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Quicksort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quicksort qs = new Quicksort();
+            int[] items = new int[]
+            {
+                8, 5, 12, 55, 3, 7, 82, 44, 35, 25, 41, 29, 17
+            };
+
+            PrintArray(items);
+
+            var sortedItems = qs.Sort(items, 0, items.Length-1);
+
+            PrintArray(sortedItems);
+
+            Console.ReadKey();
+
+        }
+
+        static void PrintArray(int[] items)
+        {
+            foreach (var item in items)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Quicksort/Quicksort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quicksort
+{
+    public class Quicksort
+    {
+        public int[] Sort(int[] items, int begin = 0, int end = 0)
+        {
+            int i = begin;
+            int j = end != 0 ? end : items.Length - 1;
+
+            int pivot = items[(begin + end) / 2];
+            int aux = 0;
+
+            while (i <= j )
+            {
+                while (items[i] < pivot && i < end)
+                {
+                    i++;
+                }
+
+                while (items[j] > pivot && j > begin)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    aux = items[i];
+                    items[i] = items[j];
+                    items[j] = aux;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (j > begin)
+            {
+                Sort(items, begin, j + 1);
+            }
+
+            if (i < end)
+            {
+                Sort(items, i, end);
+            }
+
+            return items;
+
+        }
+    }
+}
